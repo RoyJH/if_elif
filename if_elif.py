c02ea05403b4056c86761a5f65df68c716319954
Roy J. Harris III "Trey <Zithlos@gmail.com>
2022-08-30 12:40:54 -0500

    initial commit

---
+++ if_elif.py
@@ -0,0 +1,12 @@
+
+is_male = False
+is_female = False
+
+if is_male == False and is_female == False:
+    print("You identify as neither.  You're nonbinary")
+elif is_male == True and is_female == True:
+    print("Perhaps you're trans? No matter, you're loved.")
+elif is_male:
+    print("You are a male")
+elif is_female:
+    print("You are a female")
